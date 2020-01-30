2f99b8c8c0e7157d65193a0dc73af9f7bddb9b01
dkeza <dkezic@yahoo.com>
2020-01-30 21:30:56 +0100

    Show last 10 snippets

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,7 +3,6 @@ package main
 import (
 	"errors"
 	"fmt"
-	"html/template"
 	"net/http"
 	"strconv"
 
@@ -16,23 +15,33 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	files := []string{
-		app.rootDir + "ui/html/home.page.tmpl",
-		app.rootDir + "ui/html/base.layout.tmpl",
-		app.rootDir + "ui/html/footer.partial.tmpl",
-	}
-
-	ts, err := template.ParseFiles(files...)
+	s, err := app.snippets.Latest()
 	if err != nil {
 		app.serverError(w, err)
 		return
 	}
 
-	err = ts.Execute(w, nil)
-	if err != nil {
-		app.errorLog.Println(err.Error())
-		http.Error(w, "Interval Server Error", 500)
+	for _, snippet := range s {
+		fmt.Fprintf(w, "%v\n", snippet)
 	}
+
+	// files := []string{
+	// 	app.rootDir + "ui/html/home.page.tmpl",
+	// 	app.rootDir + "ui/html/base.layout.tmpl",
+	// 	app.rootDir + "ui/html/footer.partial.tmpl",
+	// }
+
+	// ts, err := template.ParseFiles(files...)
+	// if err != nil {
+	// 	app.serverError(w, err)
+	// 	return
+	// }
+
+	// err = ts.Execute(w, nil)
+	// if err != nil {
+	// 	app.errorLog.Println(err.Error())
+	// 	http.Error(w, "Interval Server Error", 500)
+	// }
 
 }
 
